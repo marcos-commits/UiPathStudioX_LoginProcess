46d3a33683a56759ec2c1fb7615da3b855808eb3
kdl <keundong27@gmail.com>
2023-12-10 16:45:44 +0900

    changed website name for the project

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace LoginProcess_Desktop
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
